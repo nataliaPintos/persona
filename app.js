27f2ddae81d39e5cd85d4d97ef6ffd2652c27645
Natália Pintos <pintoss.natalia@gmail.com>
2018-06-12 22:22:31 -0300

    adicionando descricao perfil daniela

--- app.js
+++ app.js
@@ -1,79 +1,85 @@
 angular.module("angular", [])
-    .controller("controller", ["$scope", function ($scope) {
+	.controller("controller", ["$scope", function($scope) { 
+		
+		$scope.PersonaIndex = 0;
+		$scope.Personas = [
+			{		
+				Id: 0,
+				Select: "Visually impaired",
+				Name: "Daniella",
+				Photo: "http://portaldozacarias.com.br/site/arquivos/imagens/noticias/2017/11/grande/20171122140738anaa.jpg",
+				Quote: "All that a dream needs to be fulfilled is someone who believes that it can be realized.",
+				Age: 24,
+				Occupation: "Student",
+				Family: "Single",
+				Location: "Brazil",
+				Character: "Friendly, calm and loyal.",
+				PersonalityTraits: [
+					{ Name: "Introvert/Extrovert", Value: 80 },
+					{ Name: "Analytical/Creative", Value: 50 },
+					{ Name: "Conservative/Liberal", Value: 80 },
+					{ Name: "Passive/Active", Value: 50 }
+				], 
+				Goals: ["Increase your level of autonomy.", "Graduate in law.", "Get married on the beach.", "Be a mother"],
+				Frustrations: ["Not being able to do some activities on your own", "Lack of accessibility in public places as well as in private places.", "Lack of accessibility on the web and in applications.", ],
+				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Motivations: [
+					{ Name: "Incentive", Value: 80 },
+					{ Name: "Fear", Value: 50 },
+					{ Name: "Achievement", Value: 100 },
+					{ Name: "Growth", Value: 50 },
+					{ Name: "Power", Value: 80 },
+					{ Name: "Social", Value: 70 }
+				], 
+				PreferredChannels: [
+					{ Name: "Traditional Ads", Value: 50 },
+					{ Name: "Online & Social Media", Value: 90 },
+					{ Name: "Referral", Value: 50 },
+					{ Name: "Guerrilla Efforts", Value: 30 }
+				],
 
-        $scope.PersonaIndex = 0;
-        $scope.Personas = [
-            {
-                Id: 0,
-                Select: "Visually impaired",
-                Name: "Daniella",
-                Photo: "http://portaldozacarias.com.br/site/arquivos/imagens/noticias/2017/11/grande/20171122140738anaa.jpg",
-                Quote: "A quotation that captures the essence of this person's personality",
-                Age: 24,
-                Occupation: "Being a Luckdragon",
-                Family: "A mother and 5 children",
-                Location: "The Southern Oracle",
-                Character: "Friendly, calm and loyal.",
-                PersonalityTraits: [
-                    {Name: "Introvert/Extrovert", Value: 80},
-                    {Name: "Analytical/Creative", Value: 50},
-                    {Name: "Conservative/Liberal", Value: 80},
-                    {Name: "Passive/Active", Value: 50}
-                ],
-                Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-                Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-                Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
-                Motivations: [
-                    {Name: "Incentive", Value: 50},
-                    {Name: "Fear", Value: 50},
-                    {Name: "Achievement", Value: 50},
-                    {Name: "Growth", Value: 50},
-                    {Name: "Power", Value: 50},
-                    {Name: "Social", Value: 50}
-                ],
-                PreferredChannels: [
-                    {Name: "Traditional Ads", Value: 50},
-                    {Name: "Online & Social Media", Value: 50},
-                    {Name: "Referral", Value: 50},
-                    {Name: "Guerrilla Efforts", Value: 50}
-                ]
-            },
-            {
-                Id: 1,
-                Select: "Voluntary",
-                Name: "Alexandre",
-                Photo: "https://static.pulse.ng/img/incoming/origs3879408/3806368466-w644-h960/michael-ealy1.jpg",
-                Quote: "A quotation that captures the essence of this person's personality",
-                Age: 32,
-                Occupation: "Searching for a cure for the Empress",
-                Family: "No parents, only family are the people who raised him.",
-                Location: "The Grassy Plains of Fantasia",
-                Character: "Strong, reliable and fearless.",
-                PersonalityTraits: [
-                    {Name: "Introvert/Extrovert", Value: 50},
-                    {Name: "Analytical/Creative", Value: 50},
-                    {Name: "Conservative/Liberal", Value: 50},
-                    {Name: "Passive/Active", Value: 50}
-                ],
-                Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-                Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-                Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
-                Motivations: [
-                    {Name: "Incentive", Value: 85},
-                    {Name: "Fear", Value: 50},
-                    {Name: "Achievement", Value: 50},
-                    {Name: "Growth", Value: 50},
-                    {Name: "Power", Value: 50},
-                    {Name: "Social", Value: 50}
-                ],
-                PreferredChannels: [
-                    {Name: "Traditional Ads", Value: 50},
-                    {Name: "Online & Social Media", Value: 50},
-                    {Name: "Referral", Value: 50},
-                    {Name: "Guerrilla Efforts", Value: 50}
-                ]
-            }
-        ];
-        $scope.model = $scope.Personas[0];
+				Brands: [
+					{ Name: "http://adox.com.ar/wp-content/uploads/2017/04/Whatsapp_37229.png" },
+					{ Name: "https://media.cdnandroid.com/bb/62/6f/4b/imagen-talkback-0big.jpg"},
+					{ Name: "https://png.icons8.com/color/1600/audible.png" },
+				]
+			},
+			{	
+				Id: 1,
+				Select: "Voluntary",
+				Name: "Alexandre",
+				Photo: "https://static.pulse.ng/img/incoming/origs3879408/3806368466-w644-h960/michael-ealy1.jpg",
+				Quote: "A quotation that captures the essence of this person's personality",
+				Age: 32,
+				Occupation: "Searching for a cure for the Empress",
+				Family: "No parents, only family are the people who raised him.",
+				Location: "The Grassy Plains of Fantasia",
+				Character: "Strong, reliable and fearless.",
+				PersonalityTraits: [
+					{ Name: "Introvert/Extrovert", Value: 50 },
+					{ Name: "Analytical/Creative", Value: 50 },
+					{ Name: "Conservative/Liberal", Value: 50 },
+					{ Name: "Passive/Active", Value: 50 }
+				], 
+				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
+				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
+				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Motivations: [
+					{ Name: "Incentive", Value: 50 },
+					{ Name: "Fear", Value: 50 },
+					{ Name: "Achievement", Value: 50 },
+					{ Name: "Growth", Value: 50 },
+					{ Name: "Power", Value: 50 },
+					{ Name: "Social", Value: 50 }
+				], 
+				PreferredChannels: [
+					{ Name: "Traditional Ads", Value: 50 },
+					{ Name: "Online & Social Media", Value: 50 },
+					{ Name: "Referral", Value: 50 },
+					{ Name: "Guerrilla Efforts", Value: 50 }
+				]
+			}
+		];
+		$scope.model = $scope.Personas[0];
 
-    }])
+	}])
